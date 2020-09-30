350ef1fb6fc61095ae79ec072c5a0bd3cb8dcb57
HienPham <thanhp@princeton.edu>
2020-09-30 05:55:10 -0400

    Implemented Str_copy & Str_concat

---
+++ str.h
@@ -0,0 +1,34 @@
+/*--------------------------------------------------------------------*/
+/* str.h                                                              */
+/* Author: Hien Pham                                                  */
+/*--------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+#ifndef STR_INCLUDED
+#define STR_INCLUDED
+
+/*--------------------------------------------------------------------*/
+
+/* Return the length of the string                                    */
+
+/* size_t Str_getLength(const char *pcSrc); */
+size_t Str_getLength(const char pcSrc[]);
+
+/*--------------------------------------------------------------------*/
+
+/* Copy a string to a specified location. Return the address of the   */
+/* first character of the copied string                               */
+
+/* char *Str_copy(char *pcDest, const char *pcSrc); */
+char *Str_copy(char pcDest[], const char pcSrc[]);
+
+/*--------------------------------------------------------------------*/
+
+/* Append a string to a specified string. Return the address of the   */
+/* first character of the destination string.                         */
+
+/* char *Str_concat(char *pcDest, const char *pcSrc); */
+char *Str_concat(char pcDest[], const char pcSrc[]);
+
+#endif
